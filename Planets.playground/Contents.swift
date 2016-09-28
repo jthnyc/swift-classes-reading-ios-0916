5bd3fdb284d6f6c78baee0c7541cd62cc031e3a0
Joanna Tzu-Hsuan Huang <joannathhuang@Joannas-MBP.fios-router.home>
2016-09-27 20:15:21 -0400

    Done

--- Planets.playground/Contents.swift
+++ Planets.playground/Contents.swift
@@ -8,6 +8,9 @@ let marsMoons = 2
 let marsOrbit = 686.97
 let marsLife = false
 
+// TYPICAL SETUP OF FUNCTION USING ALL FOUR PARAMETERS
+// problem is it gets tedious very fast as we use the same info request for all
+
 func planetInfo(name: String, moons: Int, orbit: Double, hasLife: Bool) {
     var moonNoun = "moon"
     if moons != 1 {
@@ -17,18 +20,22 @@ func planetInfo(name: String, moons: Int, orbit: Double, hasLife: Bool) {
     if !hasLife {
         hasLifeString = "does not have life"
     }
-    print("\(name) has \(moons) \(moonNoun). Its orbit is \(orbit) days. It \(hasLifeString).")
+    print("\(name) has \(moons)\(moonNoun). Its orbit is \(orbit) days. It \(hasLifeString)")
 }
+
+
 
 planetInfo(name: earthName, moons: earthMoons, orbit: earthOrbit, hasLife: earthLife)
 planetInfo(name: marsName, moons: marsMoons, orbit: marsOrbit, hasLife: marsLife)
 
 
+// EXAMPLE OF USING TUPLE
+// one way to make it more concise is by using tuples
 
 let earth = ("Earth", 1, 365.26, true)
 let mars = ("Mars", 2, 686.97, false)
 
-func planetInfoTuple(planet: (String, Int, Double, Bool)) {
+func planetInfoTuple(planet: (String, Int, Double, Bool)){
     var moonNoun = "moon"
     if planet.1 != 1 {
         moonNoun = "moons"
@@ -37,13 +44,14 @@ func planetInfoTuple(planet: (String, Int, Double, Bool)) {
     if !planet.3 {
         hasLifeString = "does not have life"
     }
-    print("\(planet.0) has \(planet.1) \(moonNoun). Its orbit is \(planet.2) days. It \(hasLifeString).")
+    print("\(planet.0) has \(planet.1)\(moonNoun). Its orbit is \(planet.2). It \(planet.3)")
 }
 
 planetInfoTuple(planet: earth)
 planetInfoTuple(planet: mars)
 
 
+// But that's still relatively laborsome. So consider Classes as a way to "encapsulate" several pieces of information into one object
 
 class Planet {
     var name: String
@@ -51,13 +59,16 @@ class Planet {
     var orbitalPeriod: Double
     var hasLife: Bool
 
-    init(name: String, numberOfMoons: Int, orbitalPeriod: Double, hasLife: Bool) {
+    
+    // initializer is a method called to create an instance of a class
+    
+    init(name: String, numberOfMoons: Int, orbitalPeriod: Double, hasLife: Bool){
         self.name = name
         self.numberOfMoons = numberOfMoons
         self.orbitalPeriod = orbitalPeriod
-        self.hasLife = hasLife
+        self .hasLife = hasLife
     }
-
+    
     func planetInfo() {
         var moonNoun = "moon"
         if self.numberOfMoons != 1 {
@@ -71,8 +82,16 @@ class Planet {
     }
 }
 
+
+ // creating new planets
+ // object is an instance of a class --> a thing that is the product of the class blueprint
+
+
 let earth2 = Planet(name: "Earth", numberOfMoons: 1, orbitalPeriod: 365.26, hasLife: true)
 let mars2 = Planet(name: "Mars", numberOfMoons: 2, orbitalPeriod: 685.97, hasLife: false)
+
+
+// Methods: implementing the use of class, outside of a class?
 
 func planetInfoObject(planet: Planet) {
     var moonNoun = "moon"
